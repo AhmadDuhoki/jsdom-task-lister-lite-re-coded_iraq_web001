b9e5832ae6bb20e294edd4f1bf14c33753421fbf
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-19 19:55:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     let ul = document.getElementById('tasks');
     let li = document.createElement('li');
-    
+    const taskDescription = document.getElementById('')
   })
 });
