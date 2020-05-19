9a007df85cbd8ffae850994691d338251a9ddd28
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-19 19:54:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.getElementById('submit-button').addEventListener('click', function(e){
     e.preventDefault();
-    let ul = document.getElementById('')
+    let ul = document.getElementById('tasks');
+    let li = document.createElement('li');
+    
   })
 });
