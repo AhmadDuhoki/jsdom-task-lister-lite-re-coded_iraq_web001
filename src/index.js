32ef6d183c4a04ec276ae9559119e6dc7170eb41
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-19 19:53:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElementById("submit-button").addEventListener('click', function(e){
+  document.getElementById('submit-button').addEventListener('click', function(e){
     e.preventDefault();
     let ul = document.getElementById('')
   })
